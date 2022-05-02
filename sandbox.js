1c1641605d4fe65570731b79e52d5af645494622
ASHARALORAN96 <asharoran96@gmail.com>
2022-05-03 00:58:32 +0300

    practice the basics

---
+++ sandbox.js
@@ -0,0 +1,33 @@
+var character = 'Ashar';
+var inputs = document.querySelectorAll('input');
+character = 'Ghala';
+var circle = function (diameter) {
+    return diameter * Math.PI;
+};
+console.log(circle(10));
+var obj = {
+    name: 'soso',
+    age: 26,
+    belt: 'black'
+};
+// explicit types
+var nameChar;
+var age;
+var isActive;
+var testArr = [];
+testArr.push(20);
+// union types : to could be more than one option
+var testName;
+var arr = []; // equal to [] to allow push method
+// objects
+var testObj;
+testObj = { name: 'hello' };
+testObj = [1, 2];
+// console.log(testObj)
+// strict obj
+var testObjTwo;
+testObjTwo = {
+    name: 'Nahla',
+    age: 26,
+    hasAJob: true
+};
